dc38802353a6d89290dfafc4effa1f7ddf0ccb06
asikalo2 <asikalo2@etf.unsa.ba>
2018-11-12 13:35:44 +0100

    Prepravila sam Id-eve kontrola tako da prolaze testovi za unos.

--- src/ba/unsa/etf/rpr/tutorijal05/Controller.java
+++ src/ba/unsa/etf/rpr/tutorijal05/Controller.java
@@ -1,24 +1,72 @@
 package ba.unsa.etf.rpr.tutorijal05;
 
 import javafx.event.ActionEvent;
-import javafx.scene.control.TextField;
+import javafx.scene.control.Label;
 
 public class Controller {
-    public TextField numberField;
+    public Label display;
 
     public void add1Action(ActionEvent actionEvent) {
-        numberField.setText(numberField.getText().concat("1"));
+        if (display.getText().equals("0"))
+            display.setText("");
+        display.setText(display.getText().concat("1"));
     }
 
     public void add2Action(ActionEvent actionEvent) {
-        numberField.setText(numberField.getText().concat("2"));
+        if (display.getText().equals("0"))
+            display.setText("");
+        display.setText(display.getText().concat("2"));
     }
 
     public void add3Action(ActionEvent actionEvent) {
-        numberField.setText(numberField.getText().concat("3"));
+        if (display.getText().equals("0"))
+            display.setText("");
+        display.setText(display.getText().concat("3"));
     }
 
     public void calculateAction(ActionEvent actionEvent) {
         // Do calculate
+    }
+
+    public void add4Action(ActionEvent actionEvent) {
+        if (display.getText().equals("0"))
+            display.setText("");
+        display.setText(display.getText().concat("4"));
+    }
+
+    public void add5Action(ActionEvent actionEvent) {
+        if (display.getText().equals("0"))
+            display.setText("");
+        display.setText(display.getText().concat("5"));
+    }
+
+    public void add6Action(ActionEvent actionEvent) {
+        if (display.getText().equals("0"))
+            display.setText("");
+        display.setText(display.getText().concat("6"));
+    }
+
+    public void add7Action(ActionEvent actionEvent) {
+        if (display.getText().equals("0"))
+            display.setText("");
+        display.setText(display.getText().concat("7"));
+    }
+
+    public void add8Action(ActionEvent actionEvent) {
+        if (display.getText().equals("0"))
+            display.setText("");
+        display.setText(display.getText().concat("8"));
+    }
+
+    public void add9Action(ActionEvent actionEvent) {
+        if (display.getText().equals("0"))
+            display.setText("");
+        display.setText(display.getText().concat("9"));
+    }
+
+    public void add0Action(ActionEvent actionEvent) {
+        if (display.getText().equals("0"))
+            display.setText("");
+        display.setText(display.getText().concat("0"));
     }
 }
