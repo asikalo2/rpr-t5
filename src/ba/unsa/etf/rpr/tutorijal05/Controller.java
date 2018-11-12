688a83ca59880426cdc066bc90b6945ca9c3608b
asikalo2 <asikalo2@etf.unsa.ba>
2018-11-12 13:20:41 +0100

    Napravila sam interfejs u fxmlu. Dodala sam evente za dugmad 1,2 i 3.

--- src/ba/unsa/etf/rpr/tutorijal05/Controller.java
+++ src/ba/unsa/etf/rpr/tutorijal05/Controller.java
@@ -1,5 +1,24 @@
 package ba.unsa.etf.rpr.tutorijal05;
 
-public class Controller {
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
 
+public class Controller {
+    public TextField numberField;
+
+    public void add1Action(ActionEvent actionEvent) {
+        numberField.setText(numberField.getText().concat("1"));
+    }
+
+    public void add2Action(ActionEvent actionEvent) {
+        numberField.setText(numberField.getText().concat("2"));
+    }
+
+    public void add3Action(ActionEvent actionEvent) {
+        numberField.setText(numberField.getText().concat("3"));
+    }
+
+    public void calculateAction(ActionEvent actionEvent) {
+        // Do calculate
+    }
 }
